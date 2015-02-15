9e91ab2b043e2abbbb6aac02665813b40b899e51
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-02-15 09:42:53 +0900

    Removed redundant import.

--- src/main/groovy/com/github/ksoichiro/eclipse/aar/GenerateTask.groovy
+++ src/main/groovy/com/github/ksoichiro/eclipse/aar/GenerateTask.groovy
@@ -2,9 +2,7 @@ package com.github.ksoichiro.eclipse.aar
 
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ProjectDependency
-import org.gradle.api.artifacts.PublishArtifact
 import org.gradle.api.artifacts.ResolvedDependency
 import org.gradle.api.tasks.TaskAction
 
